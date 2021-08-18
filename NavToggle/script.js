ed3a281eb472b10ee562eec934ad9dd8f27f1bbb
hhalvorsen <83094292+hhalvorsen@users.noreply.github.com>
2021-08-18 21:05:25 +0200

    Funksjonalitet fungerer, lay-out er stygg

--- NavToggle/script.js
+++ NavToggle/script.js
@@ -1,23 +1,21 @@
+// Ikoner fra font awesome
 var bar_icon = "fa-bars";
 var x_icon = "fa-times";
 
-// Ved klikk - kjør funksjon
+// Ved klikk på ikon
 document.getElementById("icon").addEventListener("click", toggle_function);
 
-// Funksjon
-    // Finn nav-list
-    // Toggle active
-    // Endre icon
 function toggle_function(){
+    // Endre ikon
     var toggle_icon = document.querySelector("#icon");
-    if (toggle_icon.classList.contains(bar_icon)){
-        toggle_icon.classList.add(x_icon);
-        toggle_icon.classList.remove(bar_icon);
+    toggle_icon.classList.toggle(bar_icon);
+    toggle_icon.classList.toggle(x_icon);
+
+    // Hent ut alle active-elements, elementer som skal endre synlighet ved klikk på ikonet
+    var active_element_list = document.querySelectorAll(".active-element");
+    // Kjør gjennom alle og endre 
+    for (x = 0; x < active_element_list.length; x++ ){
+        active_element_list[x].classList.toggle("active");
     }
-    else {
-        toggle_icon.classList.add(bar_icon);
-        toggle_icon.classList.remove(x_icon);
-    }
-    var nav_list = document.querySelector(".active-element");
-    nav_list.classList.toggle("active");
+        
 }
